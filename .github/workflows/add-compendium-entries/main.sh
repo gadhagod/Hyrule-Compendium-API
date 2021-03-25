1fb79e2c0e9c61371779466ef6c4b28bdb25307a
gadhagod <69025547+gadhagod@users.noreply.github.com>
2021-03-25 12:48:57 -0700

    Add image authentication workflow, rename add-docs workflow

---
+++ .github/workflows/add-compendium-entries/main.sh
@@ -0,0 +1,5 @@
+rockset api:documents:addDocuments botw-api creatures --body compendium/creatures.json
+rockset api:documents:addDocuments botw-api equipment --body compendium/equipment.json
+rockset api:documents:addDocuments botw-api materials --body compendium/materials.json
+rockset api:documents:addDocuments botw-api monsters --body compendium/monsters.json
+rockset api:documents:addDocuments botw-api treasure --body compendium/treasure.json
