92f1d939de0ab9c72fd2763515229eed480008f5
gadhagod <69025547+gadhagod@users.noreply.github.com>
2021-08-17 12:35:47 -0700

    Make API endpoint request demo asynchronous

--- docs/index.js
+++ docs/index.js
@@ -1,39 +1,41 @@
 var req = new XMLHttpRequest();
 
 /**
- * Creates the "Try it" button.
+ * Makes a request to a given API endpoint and writes response to @param resElemId
  * @param {string} endpoint API endpoint
  * @param {{key: string, val: string}[]} params URL parameters
  * @param {string} resElemId ID of `div` element to be manipulated
  */
 function createButton(endpoint, params, resElemId) {
     var url = `https://botw-compendium.herokuapp.com/api/v2${endpoint}?foo=foo`;
-    console.log(url);
     params.forEach((item) => {
         url += `&${encodeURIComponent(item.key)}=${encodeURIComponent(item.val)}`;
     })
 
-    req.open("GET", url, false);
-    req.send(null);
+    req.onreadystatechange = () => {
+        console.log(req.responseText);
+        res = JSON.parse(req.responseText);
 
-    res = JSON.parse(req.responseText);
+        if (!document.getElementById(resElemId + "Json")) {
+            var div = document.getElementById(resElemId);
 
-    if(!document.getElementById(resElemId + "Json")) {
-        var div = document.getElementById(resElemId);
+            var pre = document.createElement("pre");
+            pre.setAttribute("v-pre", "");
+            pre.setAttribute("data-lang", "json");
 
-        var pre = document.createElement("pre");
-        pre.setAttribute("v-pre", "");
-        pre.setAttribute("data-lang", "json");
+            var code = document.createElement("code");
+            code.setAttribute("id", resElemId + "Json");
+            code.setAttribute("class", "lang-json");
+            code.innerHTML = JSON.stringify(res, null, 4);
 
-        var code = document.createElement("code");
-        code.setAttribute("id", resElemId + "Json");
-        code.setAttribute("class", "lang-json");
-        code.innerHTML = JSON.stringify(res, null, 4);
-
-        pre.appendChild(code);
-        div.appendChild(pre);
-    } else {
-        var code  = document.getElementById(resElemId + "Json");
-        code.innerHTML = JSON.stringify(res, null, 4);;
+            pre.appendChild(code);
+            div.appendChild(pre);
+        } else {
+            var code = document.getElementById(resElemId + "Json");
+            code.innerHTML = JSON.stringify(res, null, 4);
+        }
     }
+
+    req.open("GET", url, true);
+    req.send();
 }
