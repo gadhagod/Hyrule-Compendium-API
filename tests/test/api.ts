bd9932b2f9b4ce685a8766af735fd229e6699101
gadhagod <69025547+gadhagod@users.noreply.github.com>
2021-12-19 16:15:39 -0800

    Fix master mode entry image type in tests

--- tests/test/api.ts
+++ tests/test/api.ts
@@ -197,10 +197,10 @@ describe("API response contents", () => {
             });
             it("should have working image", (done) => {
                 make_req(`/master_mode/entry/${entry}/image`, () => {
-                    let result = validateMIMEType(`out/${entry}.png`, {allowMimeTypes: ["image/png"]})
+                    let result = validateMIMEType(`out/${entry}.jpeg`, {allowMimeTypes: ["image/jpeg"]})
                     assert.equal(result.ok, true, result.error)
                     done()
-                }, {filename: `out/${entry}.png`})
+                }, {filename: `out/${entry}.jpeg`})
             });
         });
     });
