ce17014e96c9817225889887e86576e6c98ab50e
gadhagod <69025547+gadhagod@users.noreply.github.com>
2022-05-30 12:23:44 -0700

    Hardcode route versions

--- server.py
+++ server.py
@@ -1,4 +1,3 @@
-
 from src import view_funcs
 import flask
 from flask_cors import CORS
@@ -6,21 +5,42 @@ from flask_cors import CORS
 app = flask.Flask(__name__, static_folder='compendium/images')
 CORS(app)
 
-app.add_url_rule('/api/<version>', view_func=view_funcs.all)
-app.add_url_rule('/api/<version>/all', view_func=view_funcs.all)
-app.add_url_rule('/api/<version>/master_mode', view_func=view_funcs.all_master_mode)
-app.add_url_rule('/api/<version>/master_mode/all', view_func=view_funcs.all_master_mode)
+func_cnt = 0
+def load_view(view_func, version, inp=False):
+    global func_cnt
+    wrapper = (lambda inp : view_func(version, inp)) if inp else lambda : view_func(version)
+    wrapper.__name__ = str(func_cnt)
+    func_cnt += 1
+    return wrapper
 
-app.add_url_rule('/api/<version>/entry/<inp>', view_func=view_funcs.entry)
-app.add_url_rule('/api/<version>/entry/<inp>/image', view_func=view_funcs.entry_image)
-app.add_url_rule('/api/<version>/master_mode/entry/<inp>', view_func=view_funcs.master_mode_entry)
-app.add_url_rule('/api/<version>/master_mode/entry/<inp>/image', view_func=view_funcs.master_mode_entry_image)
+app.add_url_rule('/api/v1', view_func=load_view(view_funcs.all, 'v1'))
+app.add_url_rule('/api/v2', view_func=load_view(view_funcs.all, 'v2'))
+app.add_url_rule('/api/v1/all', view_func=load_view(view_funcs.all, 'v1'))
+app.add_url_rule('/api/v2/all', view_func=load_view(view_funcs.all, 'v2'))
+app.add_url_rule('/api/v1/master_mode', view_func=load_view(view_funcs.all_master_mode, 'v1'))
+app.add_url_rule('/api/v2/master_mode', view_func=load_view(view_funcs.all_master_mode, 'v2'))
+app.add_url_rule('/api/v1/master_mode/all', view_func=load_view(view_funcs.all_master_mode, 'v1'))
+app.add_url_rule('/api/v2/master_mode/all', view_func=load_view(view_funcs.all_master_mode, 'v2'))
 
-app.add_url_rule('/api/<version>/category/treasure', view_func=view_funcs.treasure)
-app.add_url_rule('/api/<version>/category/monsters', view_func=view_funcs.monsters)
-app.add_url_rule('/api/<version>/category/materials', view_func=view_funcs.materials)
-app.add_url_rule('/api/<version>/category/equipment', view_func=view_funcs.equipment)
-app.add_url_rule('/api/<version>/category/creatures', view_func=view_funcs.creatures)
+app.add_url_rule('/api/v1/entry/<inp>', view_func=load_view(view_funcs.entry, 'v1', inp=True))
+app.add_url_rule('/api/v2/entry/<inp>', view_func=load_view(view_funcs.entry, 'v2', inp=True))
+app.add_url_rule('/api/v1/entry/<inp>/image', view_func=load_view(view_funcs.entry_image, 'v1', inp=True))
+app.add_url_rule('/api/v2/entry/<inp>/image', view_func=load_view(view_funcs.entry_image, 'v2', inp=True))
+app.add_url_rule('/api/v1/master_mode/entry/<inp>', view_func=load_view(view_funcs.master_mode_entry, 'v1', inp=True))
+app.add_url_rule('/api/v2/master_mode/entry/<inp>', view_func=load_view(view_funcs.master_mode_entry, 'v2', inp=True))
+app.add_url_rule('/api/v1/master_mode/entry/<inp>/image', view_func=load_view(view_funcs.master_mode_entry_image, 'v1', inp=True))
+app.add_url_rule('/api/v2/master_mode/entry/<inp>/image', view_func=load_view(view_funcs.master_mode_entry_image, 'v2', inp=True))
+
+app.add_url_rule('/api/v1/category/treasure', view_func=load_view(view_funcs.treasure, 'v1'))
+app.add_url_rule('/api/v2/category/treasure', view_func=load_view(view_funcs.treasure, 'v2'))
+app.add_url_rule('/api/v1/category/monsters', view_func=load_view(view_funcs.monsters, 'v1'))
+app.add_url_rule('/api/v2/category/monsters', view_func=load_view(view_funcs.monsters, 'v2'))
+app.add_url_rule('/api/v1/category/materials', view_func=load_view(view_funcs.materials, 'v1'))
+app.add_url_rule('/api/v2/category/materials', view_func=load_view(view_funcs.materials, 'v2'))
+app.add_url_rule('/api/v1/category/equipment', view_func=load_view(view_funcs.equipment, 'v1'))
+app.add_url_rule('/api/v2/category/equipment', view_func=load_view(view_funcs.equipment, 'v2'))
+app.add_url_rule('/api/v1/category/creatures', view_func=load_view(view_funcs.creatures, 'v1'))
+app.add_url_rule('/api/v2/category/creatures', view_func=load_view(view_funcs.creatures, 'v2'))
 
 @app.route('/')
 @app.route('/api')
