82146d20bda04b00daa2eed0aee961bdb6b576fc
gadhagod <69025547+gadhagod@users.noreply.github.com>
2023-06-23 16:14:45 -0700

    Add 'category' fields to TOTK compendium entries

--- db/totk/mappings/equipment.sql
+++ db/totk/mappings/equipment.sql
@@ -2,5 +2,6 @@ SELECT
     * EXCEPT(_meta, _event_time),
     CAST(id as string) as _id,
     CONCAT('https://botw-compendium.herokuapp.com/api/v3/compendium/entry/', name ,'/image?game=totk') as image,
-    false as dlc
+    false as dlc,
+    'equipment' as category
 FROM _input
---
+++ db/totk/mappings/treasure.sql
@@ -0,0 +1,7 @@
+SELECT
+    * EXCEPT(_meta, _event_time),
+    CAST(id as string) as _id,
+    CONCAT('https://botw-compendium.herokuapp.com/api/v3/compendium/entry/', name ,'/image?game=totk') as image,
+    false as dlc,
+    'treasure' as category
+FROM _input
