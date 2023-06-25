4265ff936f3f504ae0fe867220e179ae9d930801
gadhagod <69025547+gadhagod@users.noreply.github.com>
2023-06-24 19:46:28 -0700

    Fix image links in totk compendium data

--- db/totk/mappings/materials.sql
+++ db/totk/mappings/materials.sql
@@ -1,7 +1,11 @@
 SELECT
     * EXCEPT(_meta, _event_time),
     CAST(id as string) as _id,
-    CONCAT('https://botw-compendium.herokuapp.com/api/v3/compendium/entry/', name ,'/image?game=totk') as image,
+    CONCAT(
+        'https://botw-compendium.herokuapp.com/api/v3/compendium/entry/', 
+        REPLACE (name, ' ', '_') ,
+        '/image?game=totk'
+    ) as image,
     false as dlc,
     'materials' as category
 FROM _input
